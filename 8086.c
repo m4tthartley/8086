a36966452f02bc936b8d4573bed534b94b23d7cb
Matt Hartley <m4tthartley@gmail.com>
2023-11-21 22:07:41 +0000

    add, sub and cmp instructions

--- 8086.c
+++ 8086.c
@@ -22,18 +22,32 @@
 
 #define OPCODE_MASK 0b11111100
 #define D_MASK 0b00000010
+#define S_MASK 0b00000010
 #define W_MASK 0b00000001
 // #define REG_MODE_MASK 0b11000000
 // #define REG_DST_MASK 0b00111000
 // #define REG_SRC_MASK 0b00000111
 
-#define OPCODE_MOV			0b10001000
-#define OPCODE_MOV_IM_RM	0b11000110
-#define OPCODE_MOV_IM		0b10110000
+#define OPCODE_MOV				0b10001000
+// #define OPCODE_MOV_IM_RM		0b11000110
+#define OPCODE_MOV_IM_RM		0b11000100 // Hoping the 2nd bit isn't important, the destination is always 1
+#define OPCODE_MOV_IM			0b10110000
 #define OPCODE_MOV_MEM_TO_ACC	0b10100000
 #define OPCODE_MOV_ACC_TO_MEM	0b10100010
-#define OPCODE_MOV_RM_SEG	0b10001110
-#define OPCODE_MOV_SEG_RM	0b10001100
+#define OPCODE_MOV_RM_SEG		0b10001110
+#define OPCODE_MOV_SEG_RM		0b10001100
+
+#define OPCODE_ADD_REG_RM		0b00000000
+#define OPCODE_ADD_IM_TO_RM		0b10000000
+#define OPCODE_ADD_IM_TO_ACC	0b00000100
+
+#define OPCODE_SUB_REG_RM		0b00101000
+#define OPCODE_SUB_IM_TO_RM		0b10000000
+#define OPCODE_SUB_IM_TO_ACC	0b00101100
+
+#define OPCODE_CMP_REG_RM		0b00111000
+#define OPCODE_CMP_IM_TO_RM		0b10000000
+#define OPCODE_CMP_IM_TO_ACC	0b00111100
 
 // typedef union {
 // 	u16
@@ -106,16 +120,59 @@ typedef struct {
 } program_t;
 
 program_t load_program(char* path) {
+	program_t result = {0};
 	core_handle_t file = core_open(path);
-	core_stat_t stat = core_stat(file);
-	u8* data = malloc(stat.size);
-	core_read(file, 0, data, stat.size);
-	core_close(file);
-	program_t result = {
-		data,
-		stat.size
-	};
+	if (file) {
+		core_stat_t stat = core_stat(file);
+		u8* data = malloc(stat.size);
+		core_read(file, 0, data, stat.size);
+		core_close(file);
+		result.instructions = data;
+		result.size = stat.size;
+	}
 	return result;
+}
+
+// char* get_rm_string(u8 opcode, u8 reg_code, u8 rm) {
+// #define get_rm_string(rm) \
+// 	if (mode == 0b11) {\
+// 		return get_register_name(rm, wide);\
+// 	} else {\
+// 		if (mode == 0 && rm == 0b110) {\
+// 			i16 disp16 = *(i16*)ip;
+// 			ip += 2;
+
+// 		} else {\
+
+// 		}\
+// 	}\
+// }
+
+#define print_rm() \
+	core_print_inline("[%s", register_names[rm_table[rm]]); \
+	if (rm < 4) { \
+		core_print_inline(" + %s", register_names[rm_plus_table[rm]]); \
+	} \
+	if (mode == 0b01) { \
+		core_print_inline(" + %hhi", disp8); \
+	} \
+	if (mode == 0b10) { \
+		core_print_inline(" + %hi", disp16); \
+	} \
+	core_print_inline("]");
+
+void print_bin8(u8 bin) {
+	char result[12] = {0};
+	// u64 result;
+	result[0] = '0';
+	result[1] = 'b';
+	result[10] = ' ';
+	FOR (i, 8) {
+		// ((char*)(&result))[i] = (1<<i) & bin ? '1' : '0';
+		result[2+i] = (1<<(7-i)) & bin ? '1' : '0';
+	}
+
+	core_print_inline(result);
 }
 
 void run_program(program_t program) {
@@ -123,7 +180,10 @@ void run_program(program_t program) {
 	while (ip < program.instructions+program.size) {
 		u8 opcode = *ip++;
 
-		// Immediate the register
+		// print 8 bit binary opcode
+		print_bin8(opcode);
+
+		// Immediate to register
 		if ((opcode & 0b11110000) == OPCODE_MOV_IM) {
 			u8 wide = (opcode & 0b1000) >> 3;
 			u8 reg = opcode & 0b111;
@@ -140,8 +200,13 @@ void run_program(program_t program) {
 			continue;
 		}
 
+		u8 opcode6 = (opcode & 0b11111100);
+
 		// Register/memory to/from register
-		if ((opcode & 0b11111100) == OPCODE_MOV) {
+		if (opcode6 == OPCODE_MOV
+			|| opcode6 == OPCODE_ADD_REG_RM
+			|| opcode6 == OPCODE_SUB_REG_RM
+			|| opcode6 == OPCODE_CMP_REG_RM) {
 			u8 reg_code = *ip++;
 			b32 dest = (opcode&D_MASK) >> 1;
 			b32 wide = 	opcode&W_MASK;
@@ -152,25 +217,38 @@ void run_program(program_t program) {
 			u8 regdst = fields[1-dest];
 			u8 regsrc = fields[dest];
 			
-			if (mode == 0b11) {
+			switch (opcode6) {
+			case OPCODE_MOV:
 				core_print_inline("mov ");
+				break;
+			case OPCODE_ADD_REG_RM:
+				core_print_inline("add ");
+				break;
+			case OPCODE_SUB_REG_RM:
+				core_print_inline("sub ");
+				break;
+			case OPCODE_CMP_REG_RM:
+				core_print_inline("cmp ");
+				break;
+			}
+			
+			if (mode == 0b11) {
+				// core_print_inline("mov ");
 				core_print_inline(get_register_name(regdst, wide));
 				core_print_inline(", %s \n", get_register_name(regsrc, wide));
 			} else  {
 				if (mode == 0 && rm == 0b110) {
 					// direct address
-					// core_error("OPCODE_MOV direct address");
-					// exit(1);
 					i16 disp16 = *(i16*)ip;
 					ip += 2;
 
 					if (dest) {
-						core_print_inline("mov ");
+						// core_print_inline("mov ");
 						core_print_inline(get_register_name(reg, wide));
 						core_print_inline(", [%hu]", disp16);
 						core_print_inline("\n");
 					} else {
-						core_print_inline("mov ");
+						// core_print_inline("mov ");
 						core_print_inline("[%hu], ", disp16);
 						core_print_inline(get_register_name(reg, wide));
 						core_print_inline("\n");
@@ -180,33 +258,15 @@ void run_program(program_t program) {
 					i16 disp16 = *(i16*)ip;
 					ip += mode;
 
-					core_print_inline("mov ");
+					// core_print_inline("mov ");
 					if (dest) {
 						core_print_inline(get_register_name(reg, wide));
-						core_print_inline(", [%s", register_names[rm_table[rm]]);
-						if (rm < 4) {
-							core_print_inline(" + %s", register_names[rm_plus_table[rm]]);
-						}
-						if (mode == 0b01) {
-							core_print_inline(" + %hhi", disp8);
-						}
-						if (mode == 0b10) {
-							core_print_inline(" + %hi", disp16);
-						}
-						core_print_inline("]");
+						core_print_inline(", ");
+						print_rm();
 						core_print_inline("\n");
 					} else {
-						core_print_inline("[%s", register_names[rm_table[rm]]);
-						if (rm < 4) {
-							core_print_inline(" + %s", register_names[rm_plus_table[rm]]);
-						}
-						if (mode == 0b01) {
-							core_print_inline(" + %hhi", disp8);
-						}
-						if (mode == 0b10) {
-							core_print_inline(" + %hi", disp16);
-						}
-						core_print_inline("], ");
+						print_rm();
+						core_print_inline(", ");
 						core_print_inline(get_register_name(reg, wide));
 						core_print_inline("\n");
 					}
@@ -215,55 +275,94 @@ void run_program(program_t program) {
 			continue;
 		}
 
+		if ((opcode & 0b11111100) == OPCODE_ADD_REG_RM) {
+
+		}
+
 		// Immediate to register/memory
-		if ((opcode & 0b11111110) == OPCODE_MOV_IM_RM) {
+		if (opcode6 == OPCODE_MOV_IM_RM
+			|| opcode6 == OPCODE_ADD_IM_TO_RM) {
 			u8 reg_code = *ip++;
-			b32 wide = 	opcode&W_MASK;
+			b32 sign = (opcode&S_MASK) >> 1;
+			// In the immediate add instruction, for some reason sign has to be 0
+			// for it to use the second byte, I don't understand why
+			b32 wide = (opcode&W_MASK);// && (opcode6==OPCODE_ADD_IM_TO_RM ? !sign : TRUE);
 			u8 mode = 	(reg_code&0b11000000) >> 6;
-			// u8 reg = 	(reg_code&0b00111000) >> 3;
+			u8 reg = 	(reg_code&0b00111000) >> 3;
 			u8 rm = 	(reg_code&0b00000111);
 			// u8 fields[] = {reg, rm};
 			// u8 regdst = fields[1-dest];
 			// u8 regsrc = fields[dest];
 
+			switch (opcode6) {
+			case OPCODE_MOV_IM_RM:
+				core_print_inline("mov ");
+				break;
+			case OPCODE_ADD_IM_TO_RM:
+				switch (reg) {
+				case 0b000:
+					core_print_inline("add ");
+					break;
+				case 0b101:
+					core_print_inline("sub ");
+					break;
+				case 0b111:
+					core_print_inline("cmp ");
+					break;
+				}
+				break;
+			}
+
 			if (mode == 0b11) {
 				i8 data8 = *ip;
 				i16 data16 = *(i16*)ip;
-				ip += 1+wide;
-				core_print_inline("mov ");
-				core_print_inline("%s,  \n", get_register_name(rm, wide));
-				if (wide) {
-					core_print_inline("word %hi", data16);
+				ip += 1 + ((opcode6==OPCODE_MOV_IM_RM) ? wide : (!sign && wide));
+				// core_print_inline("mov ");
+				core_print_inline("%s", get_register_name(rm, wide));
+				if ((opcode6==OPCODE_MOV_IM_RM) ? wide : (!sign && wide)) {
+					core_print_inline(", word %hi", data16);
 				} else {
-					core_print_inline("byte %hhi", data8);
+					core_print_inline(", byte %hhi", data8);
 				}
+				core_print_inline("\n");
 			} else {
 				i8 disp8 = *ip;
 				i16 disp16 = *(i16*)ip;
-				ip += mode;
+				ip += (mode == 0 && rm == 0b110) ? 2 : mode;
 
 				i8 data8 = *ip;
 				i16 data16 = *(i16*)ip;
-				ip += 1+wide;
+				ip += 1 + ((opcode6==OPCODE_MOV_IM_RM) ? wide : (!sign && wide));
 
 				if (mode == 0 && rm == 0b110) {
 					// direct address
-					core_error("OPCODE_MOV_IM_RM direct address");
-					exit(1);
+					// i16 disp16 = *(i16*)ip;
+					// ip += 2;
+
+					// core_print_inline(get_register_name(reg, wide));
+					core_print_inline("[%hu]", disp16);
+					if ((opcode6==OPCODE_MOV_IM_RM) ? wide : (!sign && wide)) {
+						core_print_inline(", word %hi", data16);
+					} else {
+						core_print_inline(", byte %hhi", data8);
+					}
+					core_print_inline("\n");
 				} else {
-					core_print_inline("mov ");
-					core_print_inline("[%s", register_names[rm_table[rm]]);
-					if (rm < 4) {
-						core_print_inline(" + %s", register_names[rm_plus_table[rm]]);
-					}
-					if (mode == 0b01) {
-						core_print_inline(" + %hhi", disp8);
-					}
-					if (mode == 0b10) {
-						core_print_inline(" + %hi", disp16);
-					}
-					core_print_inline("]");
-					if (wide) {
+					// core_print_inline("mov ");
+
+					// core_print_inline("[%s", register_names[rm_table[rm]]);
+					// if (rm < 4) {
+					// 	core_print_inline(" + %s", register_names[rm_plus_table[rm]]);
+					// }
+					// if (mode == 0b01) {
+					// 	core_print_inline(" + %hhi", disp8);
+					// }
+					// if (mode == 0b10) {
+					// 	core_print_inline(" + %hi", disp16);
+					// }
+					// core_print_inline("]");
+					print_rm();
+					if ((opcode6==OPCODE_MOV_IM_RM) ? wide : (!sign && wide)) {
 						core_print_inline(", word %hi", data16);
 					} else {
 						core_print_inline(", byte %hhi", data8);
@@ -308,6 +407,37 @@ void run_program(program_t program) {
 			continue;
 		}
 
+		if (opcode6 == OPCODE_ADD_IM_TO_ACC
+			|| opcode6 == OPCODE_SUB_IM_TO_ACC
+			|| opcode6 == OPCODE_CMP_IM_TO_ACC) {
+			b32 wide = 	opcode&W_MASK;
+			i16 disp16 = *(i16*)ip;
+			ip += 1+wide;
+
+			switch (opcode) {
+			case OPCODE_ADD_IM_TO_ACC:
+				core_print_inline("add ");
+				break;
+			case OPCODE_SUB_IM_TO_ACC:
+				core_print_inline("sub ");
+				break;
+			case OPCODE_CMP_IM_TO_ACC:
+				core_print_inline("cmp ");
+				break;
+			}
+			
+			if (wide) {
+				core_print_inline("ax");
+				core_print_inline(", %hi", disp16);
+			} else {
+				core_print_inline("al");
+				core_print_inline(", %hhi", disp16);
+			}
+			core_print_inline("\n");
+
+			continue;
+		}
+
 		core_error("unrecognised opcode");
 		exit(1);
 	}
@@ -320,10 +450,16 @@ int main() {
 	program_t lesson2part1 = load_program("data/listing_0039_more_movs");
 	program_t lesson2part2 = load_program("data/listing_0040_challenge_movs");
 
+	program_t lesson3 = load_program("data/listing_0041_add_sub_cmp_jnz");
+
 	// core_print("single program");
 	// run_program(single);
 
+	run_program(lesson2part1);
+	core_print("---- ---- ---- ---- ---- ---- ---- ----");
 	run_program(lesson2part2);
+	core_print("---- ---- ---- ---- ---- ---- ---- ----");
+	run_program(lesson3);
 
 	int x = 0;
 }
