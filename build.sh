f0464e47956c20ef868f82a2102b3982c25d3624
Matt Hartley <m4tthartley@gmail.com>
2023-12-27 16:16:39 +0000

    Build stuff

--- build.sh
+++ build.sh
@@ -13,9 +13,18 @@ core=$USERPROFILE/code/core/core
 include=$USERPROFILE/code/core
 warnings="-Wno-incompatible-pointer-types"
 
-# pushd build
-cl -nologo -Zi 8086.c display.c decode.c decode_format.c $core/core.c -I$include -wd4047 -wd4024 -wd4133 -link user32.lib -out:build/8086.exe
+# ../8086.c \
+# ../display.c \
+# ../decode.c \
+# ../decode_format.c \
+files="\
+../*.c \
+$core/core.c \
+"
+
+pushd build
+cl -nologo -Zi $files -I$include -wd4047 -wd4024 -wd4133 -link user32.lib -out:8086.exe
 # -Fe: build/8086.exe
-# popd
+popd
 
 # gcc 8086.c display.c decode.c decode_format.c -o build/8086.exe $core/core.c -I$include $warnings -g -O0
